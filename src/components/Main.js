e4025a49a492f9b796a32b34b3edebdadc39f6c4
1-isus <think.believe.cr8@gmain.com>
2017-09-12 22:29:21 -0500

    initial setup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,18 @@
 require('normalize.css/normalize.css');
 require('styles/App.scss');
 
-
 import Chart from './LiveChart.js';
-import Header from './Header.js';
-import Sidebar from './Sidebar';
 import React from 'react';
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <div className="col-md-12 col-xs-12 col-sm-12">
-          <div className="clearfix">
-          <div className="row">
-            <div>
-              <Header />
-            </div>
-            <div className="col-md-2 col-xs-2 col-sm-2">
-              <Sidebar />
-            </div>
-            <div className="col-md-10 col-xs-10 col-sm-10">
-              <Chart />
+        <div className="clearfix">
+          <div className="temp col-md-12 col-xs-12 col-sm-12">
+              <p className="text-center">30.5&deg;c</p>
             </div>
           </div>
-          </div>
-        </div>
       </div>
     );
   }
